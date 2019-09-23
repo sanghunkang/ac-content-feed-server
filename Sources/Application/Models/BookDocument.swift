d44d227966aa97d810e67a56bf3b1ed47ea4392f
sanghunkang <sanghunkang.dev@gmail.com>
2019-09-24 02:24:32 +0900

    it's horribly strict

--- Sources/Application/Models/BookDocument.swift
+++ Sources/Application/Models/BookDocument.swift
@@ -1,6 +1,7 @@
 import MongoKitten
 
-struct BookDocument: Document {
+// struct BookDocument: Document {
+struct BookDocument: Codable {
     let _id: String?
     var _rev: String?
     let title: String
--- Sources/Application/Routes/MongoRoutes.swift
+++ Sources/Application/Routes/MongoRoutes.swift
@@ -9,35 +9,32 @@ func initializeMongoRoutes(app: App) {
 
 extension App {
     // Define ConnectionProperties and mongoDBClient here
-    static let properties = ConnectionProperties(
-        host: "127.0.0.1",              // http address
-        port: 5984,                     // http port
-        secured: false,                 // https or http
-        username: "<mongoDB-username>", // admin username
-        password: "<mongoDB-password>"  // admin password
-    )
-    let mongoDBClient = try MongoClient("mongodb://localhost:27017")
-    static let mongoDBClient = mongoDBClient(connectionProperties: properties)
-
-    let db = client.db("myDB")
-    let collection = try db.createCollection("myCollection")
+    // static let properties = ConnectionProperties(
+    //     host: "127.0.0.1",              // http address
+    //     port: 27017,                     // http port
+    //     secured: false,                 // https or http
+    //     username: "<mongoDB-username>", // admin username
+    //     password: "<mongoDB-password>"  // admin password
+    // )
+    // let mongoDBClient = try MongoClient("mongodb://localhost:27017")
+    static let mongoDBClient = try! Database.synchronousConnect("mongodb://localhost/test")
 
 
     func mongoSaveHandler(book: BookDocument, completion: @escaping (BookDocument?, RequestError?) -> Void) {
         // Save book here
-        // App.mongoDBClient.retrieveDB("bookstore") { (database, error) in
-        //     guard let database = database  else {
-        //         return completion(nil, .internalServerError)
-        //     }
-        //     // Initialize document here
-        // }
+        print("mongo triggered")
+        print(book)
+        print(App.mongoDBClient["mottemotte"])  
+        
+        let items = App.mongoDBClient["mottemotte"]
 
-        // Save book here
-        App.mongoDBClient.db("bookstore") { (database, error) in
-            guard let database = database  else {
-                return completion(nil, .internalServerError)
-            }
-            // Initialize document here
+        let encoder = BSONEncoder()
+        do {
+            let encodedDocument: Document = try encoder.encode(book)
+            items.insert(encodedDocument)
+            print("Inserted!")
+        } catch let error {
+            print(error)
         }
     }
 
